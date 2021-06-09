53a7d83655ca1f5590483f89308e1c22223ba725
TsubakiTrillion <82280206+TsubakiTrillion@users.noreply.github.com>
2021-06-09 12:38:24 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,55 @@
+let x = 0
+input.onGesture(Gesture.Shake, function () {
+    x = randint(1, 6)
+    if (x == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (x == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . . . .
+            . # . . .
+            . . . . .
+            `)
+    } else if (x == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (x == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (x == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (x == 6) {
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            `)
+    } else {
+    	
+    }
+})
